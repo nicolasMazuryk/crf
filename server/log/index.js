0e3ca4a3f5aa76a0c38a1899c6fc2359a3250196
vlad <vo@competera.net>
2016-08-19 12:57:56 +0300

    file and console logger

--- server/log/index.js
+++ server/log/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by supervlad on 8/19/16.
  */
+
+const winston = require('winston')
+
+module.exports =  new winston.Logger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      name: 'error-file',
+      filename: './server/log/error.log',
+      level: 'error'
+    })
+  ]
+})
+
