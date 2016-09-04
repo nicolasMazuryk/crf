ea7160c57ed7974b6f221ded9772866cae3a3621
vlad <vo@competera.net>
2016-09-04 19:09:00 +0300

    local and bearer auth startegies

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,40 @@
 /**
  * Created by supervlad on 9/1/16.
  */
+
+const
+  passport = require('passport'),
+  User = require('../models/user'),
+  wrap = require('co-express'),
+  BearerStrategy = require('passport-http-bearer').Strategy,
+  LocalStrategy = require('passport-local').Strategy
+
+const local = function* (email, password, done) {
+  try {
+    const user = yield User.findOne({ email })
+    if (!user) return done(new Error('Wrong email or password'))
+    const isValid = yield user.validatePassword(password)
+    if (!isValid) return done(new Error('Wrong email or password'))
+    done(null, user)
+  }
+  catch (error) {
+    done(error)
+  }
+}
+
+const bearer = function* (token, done) {
+  try {
+    const user = yield User.findOne({ token })
+    if (!user) return done(new Error('Unauthorized'))
+    yield user.validateToken(token)
+    return done(null, user)
+  }
+  catch (error) {
+    done(error)
+  }
+}
+
+passport.use('local', new LocalStrategy({ usernameField: 'email' }, wrap(local)))
+passport.use('bearer', new BearerStrategy(wrap(bearer)))
+
+module.exports = passport
