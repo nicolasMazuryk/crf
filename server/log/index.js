7c95d9aecfb076ce5575c564bab3b95177f1f7d7
vlad <vo@competera.net>
2016-11-27 19:31:53 +0200

    debug file added

--- server/log/index.js
+++ server/log/index.js
@@ -5,23 +5,22 @@
 const winston = require('winston')
 const debug = process.argv.includes('debug')
 
-const logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      colorize: true,
-      level: 'info'
-    }),
-    new winston.transports.File({
-      name: 'error-file',
-      filename: './server/log/error.log',
-      level: 'error'
-    })
-  ]
-})
+const transports = [
+  new winston.transports.Console({
+    colorize: true,
+    level: 'info'
+  }),
+  new winston.transports.File({
+    name: 'error-file',
+    filename: './server/log/error.log',
+    level: 'error'
+  }),
+]
 
-debug && logger.add(new winston.transports.Console({
-  colorize: true,
+debug && transports.push(new winston.transports.File({
+  name: 'debug-file',
+  filename: './server/log/debug.log',
   level: 'debug'
 }))
 
-module.exports = logger
+module.exports = new winston.Logger({ transports })
